8ccc45a5993d1e1abb490168c93cec57bddb310c
WannabeSmith <iwaudbysmith@gmail.com>
2019-06-27 16:32:44 -0400

    Initial commit

---
+++ parallel_example.R
@@ -0,0 +1,13 @@
+library(Rmpi)
+library(snow)
+
+n.cores <- mpi.universe.size() - 1
+
+cl <- makeMPIcluster(n.cores) # Create the openMPI cluster
+clusterExport(cl, ls()) # Send all objects in R session to cl
+
+n.iter <- 8
+
+results.list <- parLapply(cl, 1:n.iter, function(iter){
+  print(paste("iteration", iter, "running on", mpi.get.processor.name(short = FALSE)))
+})
