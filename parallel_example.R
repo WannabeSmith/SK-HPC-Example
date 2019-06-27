bc53e765c40e0d1f4e13aca6b56861aa6280d3e0
Ian Waudby-Smith <iwaudbysmith@gmail.com>
2019-06-27 16:52:40 -0400

    Get minimal example working

--- parallel_example.R
+++ parallel_example.R
@@ -3,11 +3,18 @@ library(snow)
 
 n.cores <- mpi.universe.size() - 1
 
+print(paste("MPI universe size:", n.cores))
+
 cl <- makeMPIcluster(n.cores) # Create the openMPI cluster
 clusterExport(cl, ls()) # Send all objects in R session to cl
 
 n.iter <- 8
 
 results.list <- parLapply(cl, 1:n.iter, function(iter){
-  print(paste("iteration", iter, "running on", mpi.get.processor.name(short = FALSE)))
+  return(paste("iteration", iter, "running on", mpi.get.processor.name(short = FALSE)))
 })
+
+print(results.list)
+
+mpi.exit()
+quit()
